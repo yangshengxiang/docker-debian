51c7df30ef308b76973aaa4aa20bc45104d1e829
Vojtěch Studenka <vojtech.studenka@gmail.com>
2016-01-24 23:10:10 +0100

    Install pdo_mysql as default

--- fpm-nginx/7.0/entrypoint.sh
+++ fpm-nginx/7.0/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 
 INIT_SCRIPT=/srv/www/init.sh
@@ -6,7 +6,7 @@ INIT_SCRIPT=/srv/www/init.sh
 # if exists initial script, execute it
 if [ -f "$INIT_SCRIPT" ]; then
 	chmod +x $INIT_SCRIPT
-	exec sh $INIT_SCRIPT;
+	exec $INIT_SCRIPT
 fi
 
 exec "$@"
