fce89731436f88b62ead6c2e3701f979b7cf2b89
Vojtěch Studenka <vojtech.studenka@gmail.com>
2016-01-25 19:57:10 +0100

    Execute script directly instead exec

--- fpm-nginx/7.0/entrypoint.sh
+++ fpm-nginx/7.0/entrypoint.sh
@@ -6,8 +6,8 @@ INIT_SCRIPT=/srv/www/init.sh
 # if exists initial script, execute it
 if [ -f "$INIT_SCRIPT" ]; then
 	chmod +x $INIT_SCRIPT
-	sleep 1
-	exec $INIT_SCRIPT
+	sleep 1;
+	$INIT_SCRIPT
 fi
 
 exec "$@"
